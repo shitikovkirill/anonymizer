60bc7d70a21c561756b766b8ec2a64c8b143461b
Shitikov Kirill <sh.kiruh@gmail.com>
2019-06-03 17:15:16 +0300

    Fix script

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Run anonymize"
 sed -e "s/<HOST>/${DB_HOST}/; s/<USER>/${DB_USER}/; s/<PASS>/${DB_PASS}/" ./config/env/magento2.yml.sample > ./config/env/magento2.yml
-bundle exec rake project:anonymize[${ANONYMIZE_TYPE}]
+cp ./config/project/${ANONYMIZE_TYPE}.json ./config/project/${DB_NAME}.json
+bundle exec rake project:anonymize[${DB_NAME}]
